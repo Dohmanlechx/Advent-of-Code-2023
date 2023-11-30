190f5fd58a39cc145332cc1a3f373982ce045da0
David Öhman <35524595+Dohmanlechx@users.noreply.github.com>
2023-12-01 00:56:08 +0100

    Initial commit, prepare project

--- src/main/kotlin/Reader.kt
+++ src/main/kotlin/Reader.kt
@@ -1,9 +1,9 @@
 object Reader {
-    fun input(day: Int, split: String = "\n") =
-        lines("/${day.padded()}/input", split)
-
     fun example(day: Int, split: String = "\n") =
         lines("/${day.padded()}/example", split)
+
+    fun input(day: Int, split: String = "\n") =
+        lines("/${day.padded()}/input", split)
 
     private fun lines(path: String, split: String) =
         this@Reader::class.java
