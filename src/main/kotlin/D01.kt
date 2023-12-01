e3ccf8be452a6bc7edcec8238e2beede497c1155
David Öhman <dohman_92@hotmail.com>
2023-12-01 21:31:11 +0100

    Solve Day 01

--- src/main/kotlin/D01.kt
+++ src/main/kotlin/D01.kt
@@ -1,7 +1,60 @@
 fun main(args: Array<String>) {
-    val example = Reader.example(1)
-    print(example)
-
-    val input = Reader.input(1)
-    print(input)
+    p1()
+    p2()
 }
+
+fun p1() {
+    val lines = Reader.input(1)
+    var sum = 0
+
+    lines.forEach { line ->
+        val digits = line.filter(Char::isDigit)
+        val number = digits.convertFirstLastToNumber()
+        sum += number
+    }
+
+    assert(sum == 54927)
+}
+
+fun p2() {
+    val map = mapOf(
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "nine" to "9",
+    )
+
+    val lines = Reader.input(1)
+    var sum = 0
+
+    lines.forEach { line ->
+        var stringOfNumbers = ""
+
+        for (i in line.indices) {
+            val segment = line.substring(i)
+
+            if (segment.first().isDigit()) {
+                stringOfNumbers += segment.first()
+            } else {
+                for ((k, v) in map) {
+                    if (segment.startsWith(k)) {
+                        stringOfNumbers += v
+                        break
+                    }
+                }
+            }
+        }
+
+        val number = stringOfNumbers.convertFirstLastToNumber()
+        sum += number
+    }
+
+    assert(sum == 54581)
+}
+
+fun String.convertFirstLastToNumber() = "${first()}${last()}".toInt()
